b1622177236553d0592cb4e263377df627cb051d
Calstream <calstream.lgn@gmail.com>
2018-07-17 16:39:41 +0300

    guessing game complete

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,32 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-secret_number = rand(101)
+number = rand(101)
+
+def compare_guess(_guess, number)
+  if (_guess == "")
+    msg = ""
+    return msg
+  end
+  guess = _guess.to_i
+  msg = ""
+  if guess > number
+    if guess > number + 5
+      msg = "Way too high"
+    else msg = "Too high"
+    end
+  elsif guess == number
+    msg = "Correct"
+  elsif guess < number
+    if guess < number - 5
+      msg = "Way too low"
+    else msg = "Too low"
+    end
+  end
+  return msg
+end
 
 get '/' do
-  "The secret number is #{secret_number}."
+  msg = compare_guess(params["guess"], number)
+  erb :index, :locals => {:number => number, :msg => msg}
 end
